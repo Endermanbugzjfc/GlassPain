57caf3c3f2ba96cdf1da7fe8eba29a486b260d3d
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 01:19:19 +0800

    Added default-triggering-blocks

--- GlassPain/src/Endermanbugzjfc/GlassPain/config/GlassPainConfig.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/config/GlassPainConfig.php
@@ -10,4 +10,13 @@ class GlassPainConfig
     #[KeyName("fallback-animation")]
     public ?string $fallbackAnimation = null;
 
+    #[KeyName("default-triggering-blocks")]
+    public array $defaultTriggeringBlocks = [
+        "glass_pane",
+        "hard_glass_pane",
+        "stained_glass_pane",
+        "stained_hardened_glass_pane",
+        "iron_bars"
+    ];
+
 }
