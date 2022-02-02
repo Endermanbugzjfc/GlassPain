7b146a21f8886fe811711d5d5f2c447e9b5043ca
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 00:39:45 +0800

    Implemented open() and get()

--- GlassPain/src/Endermanbugzjfc/GlassPain/player/PlayerSession.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/player/PlayerSession.php
@@ -3,6 +3,7 @@
 namespace Endermanbugzjfc\GlassPain\player;
 
 use pocketmine\player\Player;
+use function spl_object_id;
 
 class PlayerSession
 {
@@ -13,17 +14,30 @@ class PlayerSession
     {
     }
 
-    public static function open(Player $player) : self
-    {
-
-    }
-
     /**
      * @return Player
      */
     public function getPlayer() : Player
     {
         return $this->player;
+    }
+
+    /**
+     * @var self[]
+     */
+    protected static array $sessions = [];
+
+    public static function open(Player $player) : self
+    {
+        return self::$sessions[spl_object_id($player)] = new self(
+            $player
+        );
+    }
+
+    public static function get(Player $player) : self
+    {
+        return self::$sessions[spl_object_id($player)]
+            ??= self::open($player);
     }
 
 }
