ba8d435b9f59e1b806d437ab7dbf1b97101e899d
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 00:35:21 +0800

    Register events

--- GlassPain/src/Endermanbugzjfc/GlassPain/GlassPain.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/GlassPain.php
@@ -9,6 +9,10 @@ class GlassPain extends PluginBase
 
     protected function onEnable() : void
     {
+        $this->getServer()->getPluginManager()->registerEvents(
+            new EventListener(),
+            $this
+        );
     }
 
     protected function onLoad() : void
