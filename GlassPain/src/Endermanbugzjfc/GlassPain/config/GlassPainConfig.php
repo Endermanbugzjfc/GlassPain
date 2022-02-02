becf934e29d1b0d53976cb36e8975f2c8a795c5c
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 01:33:43 +0800

    Add colours for default triggering blocks

--- GlassPain/src/Endermanbugzjfc/GlassPain/config/GlassPainConfig.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/config/GlassPainConfig.php
@@ -3,6 +3,7 @@
 namespace Endermanbugzjfc\GlassPain\config;
 
 use Endermanbugzjfc\ConfigStruct\KeyName;
+use pocketmine\block\utils\DyeColor;
 
 class GlassPainConfig
 {
@@ -11,12 +12,23 @@ class GlassPainConfig
     public ?string $defaultAnimation = null;
 
     #[KeyName("default-triggering-blocks")]
-    public array $defaultTriggeringBlocks = [
-        "glass_pane",
-        "hard_glass_pane",
-        "stained_glass_pane",
-        "stained_hardened_glass_pane",
-        "iron_bars"
-    ];
+    public array $defaultTriggeringBlocks;
+
+    public function __construct()
+    {
+        foreach ([
+                     "glass_pane",
+                     "hard_glass_pane",
+                     "stained_glass_pane",
+                     "stained_hardened_glass_pane"
+                 ] as $block
+        ) {
+            foreach (DyeColor::getAll() as $colour) {
+                $blocks[] = $colour->name() . "_" . $block;
+            }
+        }
+        $blocks[] = "iron_bars";
+        $this->defaultTriggeringBlocks = $blocks;
+    }
 
 }
