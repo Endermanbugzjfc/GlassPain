6fab0d1310baf175855c6882f671e88b7f979fe9
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 00:35:00 +0800

    Override onEnable()

--- GlassPain/src/Endermanbugzjfc/GlassPain/GlassPain.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/GlassPain.php
@@ -7,6 +7,10 @@ use pocketmine\plugin\PluginBase;
 class GlassPain extends PluginBase
 {
 
+    protected function onEnable() : void
+    {
+    }
+
     protected function onLoad() : void
     {
         self::$instance = $this;
