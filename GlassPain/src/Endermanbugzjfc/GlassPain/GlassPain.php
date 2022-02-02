9336bcca4a31a92a656d4e0de4be3217e6ed0a31
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 00:34:47 +0800

    Added singleton stuff in GlassPain.php

--- GlassPain/src/Endermanbugzjfc/GlassPain/GlassPain.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/GlassPain.php
@@ -7,4 +7,15 @@ use pocketmine\plugin\PluginBase;
 class GlassPain extends PluginBase
 {
 
+    protected function onLoad() : void
+    {
+        self::$instance = $this;
+    }
+
+    protected static self $instance;
+
+    public static function getInstance() : self {
+        return self::$instance;
+    }
+
 }
