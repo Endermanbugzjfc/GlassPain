f1005485e71700fea3e0fcc9c25df6aaabbb9f39
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 18:44:33 +0800

    Implemented TriggeringBlockPlaceEvent.php

--- GlassPain/src/Endermanbugzjfc/GlassPain/events/TriggeringBlockPlaceEvent.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/events/TriggeringBlockPlaceEvent.php
@@ -2,7 +2,34 @@
 
 namespace Endermanbugzjfc\GlassPain\events;
 
+use Endermanbugzjfc\GlassPain\player\PlayerSession;
+use pocketmine\event\block\BlockPlaceEvent;
+
 class TriggeringBlockPlaceEvent extends GlassPainEvent
 {
+
+    public function __construct(
+        protected BlockPlaceEvent $blockPlaceEvent,
+        protected PlayerSession   $playerSession
+    )
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return PlayerSession
+     */
+    public function getPlayerSession() : PlayerSession
+    {
+        return $this->playerSession;
+    }
+
+    /**
+     * @return BlockPlaceEvent
+     */
+    public function getBlockPlaceEvent() : BlockPlaceEvent
+    {
+        return $this->blockPlaceEvent;
+    }
 
 }
