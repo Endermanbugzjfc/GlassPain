63ae3041b56123657a78ed32976a54c5c63ec5bb
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 12:25:15 +0800

    Added basic properties in AnimationConfiguration.php

--- GlassPain/src/Endermanbugzjfc/GlassPain/animation/AnimationConfiguration.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/animation/AnimationConfiguration.php
@@ -2,7 +2,20 @@
 
 namespace Endermanbugzjfc\GlassPain\animation;
 
+use Endermanbugzjfc\GlassPain\GlassPainUtils;
+
 class AnimationConfiguration
 {
 
+    public string $class = "please refer to the information provided by your animation plugin";
+
+    public string $permission = GlassPainUtils::LOWERCASE_PLUGIN_NAME . ".animation.";
+
+    public array $optionPermissions = [
+        "option" => GlassPainUtils::LOWERCASE_PLUGIN_NAME . ".animation-option.",
+        "option-without-permission" => null
+    ];
+
+    public array $defaultOptionValues = [
+    ];
 }
