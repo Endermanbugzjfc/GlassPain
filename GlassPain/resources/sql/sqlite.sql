d7dc1de1b74bb193e2814ae74fac9e3698d42795
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 12:47:48 +0800

    Added table glass_pain_player

--- GlassPain/resources/sql/sqlite.sql
+++ GlassPain/resources/sql/sqlite.sql
@@ -1,1 +1,12 @@
 -- #!sqlite
+-- #{ init
+-- #    { player
+CREATE TABLE IF NOT EXISTS glass_pain_player
+(
+    uuid      BINARY(16) NOT NULL,
+    animation VARCHAR(255) DEFAULT NULL,
+
+    PRIMARY KEY (uuid)
+);
+-- #    }
+-- #}
