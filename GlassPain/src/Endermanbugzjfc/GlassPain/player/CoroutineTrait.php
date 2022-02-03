ecd05660dd27b89f22f9290902437f27bb492068
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 18:51:07 +0800

    Implemented concurrentLoop()

--- GlassPain/src/Endermanbugzjfc/GlassPain/player/CoroutineTrait.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/player/CoroutineTrait.php
@@ -8,4 +8,19 @@ use SOFe\AwaitStd\Await;
 trait CoroutineTrait
 {
 
+    public function concurrentLoop(
+        Closure $run
+    ) : void {
+        Await::f2c(function() use
+        (
+            $run
+        ) {
+            while (true) {
+                if ($run() !== null) {
+                    return;
+                }
+            }
+        });
+    }
+
 }
