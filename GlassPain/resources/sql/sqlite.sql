6e0f940e7ab18f1f39af54fd0c899c1eaa124b39
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 12:58:58 +0800

    Upgraded table player to player_animation_config

--- GlassPain/resources/sql/sqlite.sql
+++ GlassPain/resources/sql/sqlite.sql
@@ -1,12 +1,14 @@
 -- #!sqlite
 -- #{ init
--- #    { player
-CREATE TABLE IF NOT EXISTS glass_pain_player
+-- #    { player_animation_config
+CREATE TABLE IF NOT EXISTS glass_pain_player_animation_config
 (
-    uuid      BINARY(16) NOT NULL,
-    animation VARCHAR(255) DEFAULT NULL,
+    uuid        BINARY(16)   NOT NULL,
+    animation   VARCHAR(255) NOT NULL,
+    config      TEXT            DEFAULT NULL,
+    last_enable BIGINT UNSIGNED DEFAULT NULL, -- time()
 
-    PRIMARY KEY (uuid)
+    PRIMARY KEY (uuid, animation)
 );
 -- #    }
 -- #}
