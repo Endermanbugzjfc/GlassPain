6d6770828e37fe33e2a068309235a3a9c1fa5920
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-03 19:04:18 +0800

    Extracted PlayerSessionManagerTrait.php from PlayerSession.php

--- GlassPain/src/Endermanbugzjfc/GlassPain/player/PlayerSession.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/player/PlayerSession.php
@@ -8,16 +8,14 @@ use Generator;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\EventPriority;
 use pocketmine\player\Player;
-use pocketmine\Server;
 use SOFe\AwaitStd\AwaitStd;
-use function array_diff;
-use function array_keys;
 use function spl_object_id;
 
 class PlayerSession
 {
     use TriggeringBlocksManagerTrait,
-        CoroutineTrait;
+        CoroutineTrait,
+        PlayerSessionManagerTrait;
 
     protected DataProvider $dataProvider;
 
@@ -75,50 +73,6 @@ class PlayerSession
     public function getPlayer() : Player
     {
         return $this->player;
-    }
-
-    /**
-     * @var self[]
-     */
-    protected static array $sessions = [];
-
-    public static function open(
-        Player $player,
-        bool   $clean = false
-    ) : self
-    {
-        if ($clean) {
-            foreach (
-                Server::getInstance()->getOnlinePlayers()
-                as $sPlayer
-            ) {
-                if ($sPlayer === $player) {
-                    continue;
-                }
-                if (self::get($sPlayer) === null) {
-                    self::open($player);
-                }
-                $ids[] = spl_object_id($sPlayer);
-            }
-            $leakedSessionsIds = array_diff(
-                array_keys(self::$sessions),
-                $ids ?? []
-            );
-            foreach ($leakedSessionsIds as $leakedSessionsId) {
-                self::$sessions[$leakedSessionsId]->close(true);
-            }
-        }
-        return self::$sessions[spl_object_id($player)] = new self(
-            $player
-        );
-    }
-
-    public static function get(
-        Player $player
-    ) : self
-    {
-        return self::$sessions[spl_object_id($player)]
-            ??= self::open($player, true);
     }
 
 }
---
+++ GlassPain/src/Endermanbugzjfc/GlassPain/player/PlayerSessionManagerTrait.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Endermanbugzjfc\GlassPain\player;
+
+use pocketmine\player\Player;
+use pocketmine\Server;
+use function array_diff;
+use function array_keys;
+use function spl_object_id;
+
+trait PlayerSessionManagerTrait
+{
+
+    /**
+     * @var self[]
+     */
+    protected static array $sessions = [];
+
+    public static function open(
+        Player $player,
+        bool   $clean = false
+    ) : self
+    {
+        if ($clean) {
+            foreach (
+                Server::getInstance()->getOnlinePlayers()
+                as $sPlayer
+            ) {
+                if ($sPlayer === $player) {
+                    continue;
+                }
+                if (self::get($sPlayer) === null) {
+                    self::open($player);
+                }
+                $ids[] = spl_object_id($sPlayer);
+            }
+            $leakedSessionsIds = array_diff(
+                array_keys(self::$sessions),
+                $ids ?? []
+            );
+            foreach ($leakedSessionsIds as $leakedSessionsId) {
+                self::$sessions[$leakedSessionsId]->close(true);
+            }
+        }
+        return self::$sessions[spl_object_id($player)] = new self(
+            $player
+        );
+    }
+
+    public static function get(
+        Player $player
+    ) : self
+    {
+        return self::$sessions[spl_object_id($player)]
+            ??= self::open($player, true);
+    }
+
+    protected function close(
+        bool $clean = false
+    ) : void
+    {
+    }
+
+}
