0f19c14064ee7c421ce3b3a00e66d6d604c74565
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-05 04:34:52 +0800

    Added display name as info

--- GlassPain/src/Endermanbugzjfc/GlassPain/animation/AnimationInfo.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/animation/AnimationInfo.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\GlassPain\animation;
 
+use Endermanbugzjfc\GlassPain\Utils;
+use pocketmine\Server;
+use SOFe\InfoAPI\CommonInfo;
 use SOFe\InfoAPI\Info;
+use SOFe\InfoAPI\InfoAPI;
+use SOFe\InfoAPI\NumberInfo;
+use SOFe\InfoAPI\PlayerInfo;
+use SOFe\InfoAPI\StringInfo;
 
 final class AnimationInfo extends Info
 {
@@ -21,7 +28,13 @@ final class AnimationInfo extends Info
 
     public static function init() : void
     {
-
+        $glassPain = Utils::LOWERCASE_PLUGIN_NAME;
+        InfoAPI::provideInfo(
+            self::class,
+            StringInfo::class,
+            "$glassPain.animation.displayName",
+            fn(self $info) => $info->getValue()->displayName
+        );
     }
 
     /**
