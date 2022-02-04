07619d0bddfb579846fb567b195c69111f85e9fa
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-05 03:59:29 +0800

    Added speed multiplier stuff

--- GlassPain/src/Endermanbugzjfc/GlassPain/animation/AnimationBase.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/animation/AnimationBase.php
@@ -5,4 +5,17 @@ namespace Endermanbugzjfc\GlassPain\animation;
 abstract class AnimationBase
 {
 
+    protected array $options = [];
+
+    public function getDefaultOptions() : array
+    {
+        return [
+            "speed" => $this->getDefaultSpeedMultiplier()
+        ];
+    }
+
+    abstract protected function getMinimumSpeedMultiplier() : float;
+
+    abstract protected function getDefaultSpeedMultiplier() : float;
+
 }
