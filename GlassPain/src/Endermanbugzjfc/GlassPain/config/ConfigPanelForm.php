6f2343583e99836032844c5b87e0131d90571edb
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-05 04:48:18 +0800

    Added $title and $content

--- GlassPain/src/Endermanbugzjfc/GlassPain/config/ConfigPanelForm.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/config/ConfigPanelForm.php
@@ -5,4 +5,11 @@ namespace Endermanbugzjfc\GlassPain\config;
 class ConfigPanelForm
 {
 
+    public string $title = "{bold}{darkBlue}Glass{gold}Pain";
+
+    public string $content = <<<EOT
+    {yellow}Selected animation: {white}{animation.displayName}
+    EOT;
+
+
 }
