ac6358da69c74961c2747427d21df98a956665a5
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-05 03:44:56 +0800

    Imma recode AnimationBase.php

--- GlassPain/src/Endermanbugzjfc/GlassPain/animation/AnimationBase.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/animation/AnimationBase.php
@@ -2,15 +2,7 @@
 
 namespace Endermanbugzjfc\GlassPain\animation;
 
-use Endermanbugzjfc\GlassPain\player\PlayerSession;
-
 abstract class AnimationBase
 {
-
-    abstract public static function load(
-        ?array $options
-    ) : self;
-
-    abstract public function getPlayDuration() : float;
 
 }
