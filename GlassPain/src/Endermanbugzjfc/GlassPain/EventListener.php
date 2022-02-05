74abf3399f35ce3a39a75fcfc36656e264878100
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-06 00:24:56 +0800

    Track new players in EventListener.php

--- GlassPain/src/Endermanbugzjfc/GlassPain/EventListener.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/EventListener.php
@@ -4,15 +4,27 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\GlassPain;
 
+use Closure;
 use Endermanbugzjfc\GlassPain\player\PlayerSession;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDataSaveEvent;
 use pocketmine\event\player\PlayerLoginEvent;
 
 class EventListener implements Listener
 {
 
-    public function __construct()
+    public function __construct(
+        protected Closure $onNewPlayer
+    )
     {
+    }
+
+    public function onPlayerDataSaveEvent(PlayerDataSaveEvent $event) : void
+    {
+        if ($event->getPlayer()->hasPlayedBefore()) {
+            return;
+        }
+        ($this->onNewPlayer)();
     }
 
     public function onPlayerLoginEvent(PlayerLoginEvent $event) : void
