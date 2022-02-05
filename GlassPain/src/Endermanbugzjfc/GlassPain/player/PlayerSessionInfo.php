9446d24547a8254d6e846c4cb27843054cfbc457
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-06 00:58:53 +0800

    Implemented toString()

--- GlassPain/src/Endermanbugzjfc/GlassPain/player/PlayerSessionInfo.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/player/PlayerSessionInfo.php
@@ -8,6 +8,7 @@ use Endermanbugzjfc\GlassPain\GlassPain;
 use SOFe\InfoAPI\Info;
 use SOFe\InfoAPI\InfoAPI;
 use SOFe\InfoAPI\NumberInfo;
+use SOFe\InfoAPI\PlayerInfo;
 use SOFe\InfoAPI\RatioInfo;
 use SOFe\InfoAPI\StringInfo;
 
@@ -21,6 +22,9 @@ final class PlayerSessionInfo extends Info
 
     public function toString() : string
     {
+        return (new PlayerInfo(
+            $this->getValue()->getPlayer()
+        ))->toString();
     }
 
     public static function init() : void
