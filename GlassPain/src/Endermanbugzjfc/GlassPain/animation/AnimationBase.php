78ea7f48098799ca694913fbfc27644bdb3d50fa
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-05 23:41:16 +0800

    Set getDefaultOptions() to final

--- GlassPain/src/Endermanbugzjfc/GlassPain/animation/AnimationBase.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/animation/AnimationBase.php
@@ -7,7 +7,7 @@ abstract class AnimationBase
 
     protected array $options = [];
 
-    public function getDefaultOptions() : array
+    final public function getDefaultOptions() : array
     {
         $return["speed"] = 1.0;
         return $return;
