32b4ea2e7d8d1ccc7c607f42d851050fff30e058
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-05 23:46:18 +0800

    Renamed the original InstantAnimation.php to NoAnimation.php

---
+++ GlassPain/src/Endermanbugzjfc/GlassPain/animation/presets/instant/NoAnimation.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\GlassPain\animation\presets\instant;
+
+use Endermanbugzjfc\GlassPain\animation\InstantAnimation;
+
+class NoAnimation extends InstantAnimation
+{
+
+}
