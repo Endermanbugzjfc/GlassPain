44c446cbb8b3a94dc2911c07eb2ad76b5369f33b
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-06 00:15:45 +0800

    Added animation entry config fields

--- GlassPain/src/Endermanbugzjfc/GlassPain/config/ConfigOverviewForm.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/config/ConfigOverviewForm.php
@@ -11,5 +11,16 @@ class ConfigOverviewForm extends ConfigForm
     {YELLOW}You owns {Player OwnedAnimationsCount} animations.
     EOT;
 
+    public ?string $AnimationEntryIcon = <<<EOT
+    {Animation Icon}
+    EOT; // TODO: Null icon
+
+
+    public string $AnimationEntryText = <<<EOT
+    {BOLD}{DARKBLUE}{Animation DisplayName}
+    {BLUE}{Animation PopularityCount} players are using
+    EOT;
+
+
 
 }
