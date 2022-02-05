8e84b7c1effe31984afa692302e28e3ee70544f7
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-05 23:47:24 +0800

    Renamed instant namespace to no

--- GlassPain/src/Endermanbugzjfc/GlassPain/animation/presets/no/NoAnimation.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/animation/presets/no/NoAnimation.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Endermanbugzjfc\GlassPain\animation\presets\instant;
+namespace Endermanbugzjfc\GlassPain\animation\presets\no;
 
 use Endermanbugzjfc\GlassPain\animation\InstantAnimation;
 
