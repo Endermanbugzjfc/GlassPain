38f56d262f9b7bd8908be03ae2f5e8444b816b7a
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-06 00:04:38 +0800

    Added $Content of ConfigOverviewForm.php

--- GlassPain/src/Endermanbugzjfc/GlassPain/config/ConfigOverviewForm.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/config/ConfigOverviewForm.php
@@ -7,4 +7,9 @@ namespace Endermanbugzjfc\GlassPain\config;
 class ConfigOverviewForm extends ConfigForm
 {
 
+    public string $Content = <<<EOT
+    {YELLOW}You owns {Player OwnedAnimationsCount} animations.
+    EOT;
+
+
 }
