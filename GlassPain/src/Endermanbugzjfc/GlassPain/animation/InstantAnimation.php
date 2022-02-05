487020cd065e8e907e25dac24dd561bd8d290e5b
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-05 23:39:46 +0800

    Lock duration getter functions in InstantAnimation.php

--- GlassPain/src/Endermanbugzjfc/GlassPain/animation/InstantAnimation.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/animation/InstantAnimation.php
@@ -5,4 +5,12 @@ namespace Endermanbugzjfc\GlassPain\animation;
 abstract class InstantAnimation extends AnimationBase
 {
 
+    final protected function getMinimumPlayDuration() : float {
+        return 0;
+    }
+
+    final protected function getDefaultPlayDuration() : float {
+        return 0;
+    }
+
 }
