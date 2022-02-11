cee1c27669b5fff3a8401335c1759d800e493256
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-02-12 01:59:00 +0800

    Implemented sendPanelForm()

--- GlassPain/src/Endermanbugzjfc/GlassPain/player/FormSession.php
+++ GlassPain/src/Endermanbugzjfc/GlassPain/player/FormSession.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\GlassPain\player;
 
+use Endermanbugzjfc\GlassPain\GlassPain;
+use Generator;
+use SOFe\AwaitStd\Await;
+use SOFe\InfoAPI\InfoAPI;
+use Vecnavium\FormsUI\CustomForm;
+
 class FormSession
 {
 
@@ -11,11 +17,47 @@ class FormSession
         protected PlayerSession $playerSession
     )
     {
-        $this->overviewForm();
+        $this->panelForm();
     }
 
-    protected function overviewForm() : void
+    protected function panelForm() : Generator
     {
+        $this->sendPanelForm(yield Await::RESOLVE);
+        $data = yield from Await::ONCE;
+    }
+
+    protected function sendPanelForm(callable $callback) : void
+    {
+        $form = new CustomForm($callback);
+        $config = GlassPain::getInstance()->config->PanelForm;
+        $info = $this->getPlayerSession()->getInfo();
+        $form->setTitle(InfoAPI::resolve(
+            $config->Title,
+            $info
+        ));
+        $animationDefault = $this->animationDefault
+            ??= $this->getPlayerSession()->getAnimation();
+        foreach (
+            $this->getPlayerSession()->getAvailableAnimations()
+            as $index => $animation
+        ) {
+            $animations[] = $animation->getConfig()->parseDisplayName();
+            if ($animation === $animationDefault) {
+                $animationDefaultIndex = $index;
+            }
+        }
+        $form->addDropdown(InfoAPI::resolve(
+            $config->AnimationDropdownLabel,
+            $info
+        ), $animations ?? [], $animationDefaultIndex ?? null);
+        $form->addInput(InfoAPI::resolve(
+            $config->AnimationSearchBarLabel,
+            $info
+        ), InfoAPI::resolve(
+            $config->AnimationSearchBarPlaceholder,
+            $info
+        ), $animationDefault->getConfig()->DisplayName);
+        $this->getPlayerSession()->getPlayer()->sendForm($form);
     }
 
     /**
